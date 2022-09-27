c9d7385321dd06855d130becc18f0ab499ce382e
neta <neta111.shefer@gmail.com>
2022-09-27 14:33:04 +0300

    added excel route for parsing

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import Fastify from 'fastify'
+import { PORT } from './config'
+import excelRoute from './routes/excelRoute';
+import cors from '@fastify/cors'
+
+const fastify = Fastify()
+
+fastify.register(excelRoute, { prefix: "excel" });
+
+const start = async () => {
+    try {
+        await fastify.listen({ port: PORT });
+        console.log(`server is listening on port ${PORT}`);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+}
+
+start();
