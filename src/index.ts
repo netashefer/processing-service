b16c85d2e34758a1ef88d638a39338b64641e388
Yahell <yahellodev@gmail.com>
2022-10-09 20:36:38 +0300

    make https

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
 import cors from '@fastify/cors';
 import Fastify from 'fastify';
+import fs from 'fs';
+import path from 'path';
 import { PORT } from './config';
 import rootRouter from './routes';
-import dbConnector from './db/db';
 
-const fastify = Fastify();
-fastify.register(cors, { origin: true });
+const fastify = Fastify({https: {
+	key: fs.readFileSync(path.join(__dirname, 'cert.key')),
+	cert: fs.readFileSync(path.join(__dirname, 'cert.crt'))
+  }});
+fastify.register(cors, {origin: true});
 fastify.register(rootRouter);
-fastify.register(dbConnector);
 
 const start = async () => {
     try {
         await fastify.listen({ port: PORT });
         console.log(`server is listening on port ${PORT}`);
     } catch (err) {
-        console.log(err);
         process.exit(1);
     }
 };
