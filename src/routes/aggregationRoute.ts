99c555b0b109e85bf7020152953d0138fbff3470
neta <neta111.shefer@gmail.com>
2022-11-12 09:34:02 +0200

    cr fixes

--- src/routes/aggregationRoute.ts
+++ src/routes/aggregationRoute.ts
@@ -1,10 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { aggregationService } from "../services/aggregationService";
-import { excelService } from "../services/excelService";
 import { GraphConfig } from "../types/graph.types";
 
 const aggregationRoute = async (fastify: FastifyInstance) => {
-
     fastify.post<{ Body: { dataSourceId: string, graphConfig: GraphConfig; }; }>('/data', async (request, reply) => {
         const { graphConfig, dataSourceId } = request.body;
         return await aggregationService.runAggregation(graphConfig, dataSourceId);
