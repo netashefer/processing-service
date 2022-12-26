53c84ab6aaa7bf20e4a7024d8bccf2811580ed7d
neta <neta111.shefer@gmail.com>
2022-12-26 14:45:24 +0200

    add word cloud aggregation

--- src/types/graph.types.ts
+++ src/types/graph.types.ts
@@ -2,4 +2,5 @@ export type GraphConfig = {
     x_field: string,
     y_field: { aggragation?: "valuesCount" | "uniqueValues"; field?: string; };
     dataFields?: string[];
+    dataFieldsAggregation: 'weight' | null;
 };
