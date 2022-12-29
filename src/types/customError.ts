4c5ace63aaecb83c3028e55710d91f53387d1c79
neta <neta111.shefer@gmail.com>
2022-12-29 20:40:01 +0200

    remove commens

--- src/types/customError.ts
+++ src/types/customError.ts
@@ -4,13 +4,11 @@ class CustomError extends Error {
     constructor(message: string, invalidFields: string[]) {
         super(message);
 
-        // assign the error class name in your custom error (as a shortcut)
         this.name = this.constructor.name;
 
-        // capturing the stack trace keeps the reference to your error class
+        // capturing the stack trace keeps the reference to error class
         Error.captureStackTrace(this, this.constructor);
 
-        // you may also assign additional properties to your error
         this.invalidFields = invalidFields;
     }
 }
